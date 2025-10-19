52eef14e0868877ae29c9e60f805c8864b5b3a63
Jin <jxchan14@gmail.com>
2025-10-19 13:26:58 +0100

    Set up
    setting up all the folders and downloaded libraries and files needed to start making the visualiser

---
+++ TuringMachine/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Turing Machine Simulator</h1>
+      <p>Welcome! This is a simple starter page.</p>
+    </div>
+  );
+}
+
+export default App;
